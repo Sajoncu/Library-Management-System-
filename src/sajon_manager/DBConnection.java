12c878d70576b36eda3e948efef3e8eb92d05d7d
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2018-11-28 01:45:13 +0600

    Done sign up mechanism

---
+++ src/sajon_manager/DBConnection.java
@@ -0,0 +1,30 @@
+package sajon_manager;
+import java.sql.*;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Sazon
+ */
+public class DBConnection {
+    Connection con;
+    
+    public static Connection ConnectDB()
+    {
+        try
+        {
+            //Register
+            Class.forName("org.sqlite.JDBC");
+            
+            //Create connection
+            Connection con = DriverManager.getConnection("jdbc:sqlite:D:\\Harvert Shild\\LibraryManagementSystem\\LibraryManager.sqlite");
+            return con;
+        } 
+        catch (Exception e){
+        
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
